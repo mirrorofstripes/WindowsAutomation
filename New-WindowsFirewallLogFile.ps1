dc3a9e4bf97b1ed599ad6c5e4a67bff4352b942e
mirrorofstripes <73394315+mirrorofstripes@users.noreply.github.com>
2020-10-24 16:47:30 +0200

    Add files via upload

---
+++ New-WindowsFirewallLogFile.ps1
@@ -0,0 +1,26 @@
+﻿Function New-WindowsFirewallLogFile {
+
+    param (
+        [Parameter(Mandatory=$true)][String]$Path,
+        [Switch]$Force
+    )
+    
+    # If the target logfile doesn't exist or it does and -Force has been passed
+    If ((!(Test-Path -Path $Path)) -or ((Test-Path -Path $Path) -and $Force )) {
+        
+        # Remove the file we're about to recreate if there is any
+        Remove-Item -Force -Verbose -Path $Path -ErrorAction SilentlyContinue
+        
+        # Create the new file
+        New-Item -Force -Path $Path -ItemType File
+
+        # Populate the file's DACL 
+        Start-Process -NoNewWindow -Wait -FilePath "C:\Windows\System32\icacls.exe" -ArgumentList "$($Path) /inheritance:R"
+        Start-Process -NoNewWindow -Wait -FilePath "C:\Windows\System32\icacls.exe" -ArgumentList "$($Path) /grant `"NT AUTHORITY\SYSTEM`":F"
+        Start-Process -NoNewWindow -Wait -FilePath "C:\Windows\System32\icacls.exe" -ArgumentList "$($Path) /grant BUILTIN\Administrators:F"
+        Start-Process -NoNewWindow -Wait -FilePath "C:\Windows\System32\icacls.exe" -ArgumentList "$($Path) /grant `"BUILTIN\Network Configuration Operators`":F"
+        Start-Process -NoNewWindow -Wait -FilePath "C:\Windows\System32\icacls.exe" -ArgumentList "$($Path) /grant `"NT SERVICE\MpsSvc`":F"
+
+    }
+
+}
